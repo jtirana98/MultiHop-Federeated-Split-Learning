34a081aeaed8fcfbf83c3d7a321063c9b8257ead
Joana Tirana <jtirana@uth.gr>
2023-02-02 19:35:41 +0200

    s

--- pipeline_simulation/systemAPI.cpp
+++ pipeline_simulation/systemAPI.cpp
@@ -219,11 +219,10 @@ void systemAPI::refactor(refactoring_data refactor_message) {
                 continue;
             }*/
 
-
-            if(addr.first > 5) {
+            if(addr.first >= (int)my_network_layer.rooting_table.size()-2) {
                 std::pair<std::string, int> my_addr = my_network_layer.rooting_table.find(0)->second;
                 int my_port = my_addr.second;
-                my_port = my_port + (addr.first - 5);
+                my_port = my_port + (addr.first - my_network_layer.rooting_table.size());
                 my_network_layer.rooting_table.insert({addr.first, std::pair<std::string, int>(addr.second, my_port)});
             }
             else {
