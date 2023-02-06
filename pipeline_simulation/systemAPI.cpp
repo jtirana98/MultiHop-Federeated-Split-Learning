332d98bb479a9feaa18d76e691cf3df28406e3b4
Joana Tirana <jtirana@uth.gr>
2023-02-06 13:59:09 +0200

    error fix

--- pipeline_simulation/systemAPI.cpp
+++ pipeline_simulation/systemAPI.cpp
@@ -228,7 +228,7 @@ void systemAPI::refactor(refactoring_data refactor_message) {
             else if (addr.first > 28) {
                 std::pair<std::string, int> my_addr = my_network_layer.rooting_table.find(28)->second;
                 int my_port = my_addr.second;
-                my_port = my_port + (28 - addr.first);
+                my_port = my_port + (addr.first-28);
                 my_network_layer.rooting_table.insert({addr.first, std::pair<std::string, int>(addr.second, my_port)});
             }
             else {
