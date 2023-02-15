ba3add5937583dd61aae44c02fd5a5cd504e440b
root <root@localhost.localdomain>
2023-02-15 17:01:42 +0200

    40 1

--- pipeline_simulation/profiling/data_owner_simulated.cpp
+++ pipeline_simulation/profiling/data_owner_simulated.cpp
@@ -105,10 +105,10 @@ int main(int argc, char **argv) {
                 my_port = my_port + (data_owners[i] +3);
                 sys_.my_network_layer.rooting_table.insert({data_owners[i], std::pair<std::string, int>(my_addr.first, my_port)});
             }
-            else if(data_owners[i] > 23) {
-                std::pair<std::string, int> my_addr = sys_.my_network_layer.rooting_table.find(23)->second;
+            else if(data_owners[i] > 13) {
+                std::pair<std::string, int> my_addr = sys_.my_network_layer.rooting_table.find(13)->second;
                 int my_port = my_addr.second;
-                my_port = my_port + (data_owners[i] - 23);
+                my_port = my_port + (data_owners[i] - 13);
                 sys_.my_network_layer.rooting_table.insert({data_owners[i], std::pair<std::string, int>(my_addr.first, my_port)});
             }
 
